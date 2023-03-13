73b16cab8ac7c67bc212e5b169a4232644c09ce0
vbetsch <vic.betsch@gmail.com>
2023-03-13 01:01:38 +0100

    :lipstick: Profile UI

--- apps/kickflip-webui/pages/profile.tsx
+++ apps/kickflip-webui/pages/profile.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { userContext } from "../context/UserProvider"
 import { authContext } from "../context/AuthProvider"
+import { Title } from "../components/Title"
 
 // eslint-disable-next-line import/no-default-export
 export default function Index() {
@@ -8,25 +9,30 @@ export default function Index() {
     const { disconnect } = useContext(authContext)
     return (
         <div className="card">
-            <p>
-                <b>Username</b>: {user.userName}
-            </p>
-            <p>
-                <b>Fistname</b>: {user.firstName}
-            </p>
-            <p>
-                <b>Lastname</b>: {user.lastName}
-            </p>
-            <p>
-                <b>Email</b>: {user.email}
-            </p>
-            <p>
-                <b>Phone</b>: {user.phone}
-            </p>
-            <div className="form-validate">
-                <button className="button" onClick={disconnect}>
-                    Logout
-                </button>
+            <Title content="Profile" />
+            <div className="form">
+                <div className="form-fields">
+                    <p>
+                        <b>Username</b>: {user.userName}
+                    </p>
+                    <p>
+                        <b>Fistname</b>: {user.firstName}
+                    </p>
+                    <p>
+                        <b>Lastname</b>: {user.lastName}
+                    </p>
+                    <p>
+                        <b>Email</b>: {user.email}
+                    </p>
+                    <p>
+                        <b>Phone</b>: {user.phone}
+                    </p>
+                </div>
+                <div className="form-validate">
+                    <button className="button" onClick={disconnect}>
+                        Logout
+                    </button>
+                </div>
             </div>
         </div>
     )
